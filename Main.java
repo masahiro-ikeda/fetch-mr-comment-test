c85bdcb8339d5786241024d157348d98288f7aad
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2022-06-09 01:08:09 +0900

    FizzBuzzの実装

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!");
+
+        for (int i = 1; i <= 100; i++) {
+
+            System.out.printf("%3d: ", i);
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz!!");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz!!");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz!!");
+            } else {
+                System.out.println();
+            }
+
+        }
     }
 }
